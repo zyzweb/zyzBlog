898cb8e3c72639d7295ba398ecbea5e63925ae01
zyzweb <zyz1609062132@gmail.com>
2022-06-20 12:31:05 +0800

    修改 deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:mqyqingfeng/learn-typescript.git master:gh-pages
+git push -f https://github.com/zyzweb/zyzBlog.git master:gh-pages
 
 cd -
